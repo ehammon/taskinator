e6489b36b2812ad3527230361c8250c75498330f
Emily Hammon <ehammon7@gmail.com>
2021-11-17 21:21:35 -0500

    created new array with for/if statements to delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
+var tasks = [];
 
 var taskFormHandler = function(event) { 
   event.preventDefault(); 
@@ -28,10 +29,13 @@ var taskFormHandler = function(event) {
   else {
     var taskDataObj = {
       name: taskNameInput,
-      type: taskTypeInput
+      type: taskTypeInput,
+      status: "to do"
     };
-
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
     createTaskEl(taskDataObj);
+    
   }
 };
 
@@ -41,13 +45,22 @@ var completeEditTask = function(taskName, taskType, taskId) {
   //set new values
   taskSelected.querySelector("h3.task-name").textContent = taskName;
   taskSelected.querySelector("span.task-type").textContent - taskType;
+  //alert("Task Updated!");
 
-  alert("Task Updated!");
+  //loop through tasks array and task object with new content
+  for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === parseInt(taskId)) {
+      tasks[i].name = taskName;
+      tasks[i].type = taskType;
+    }
+  };
+  
 
   formEl.removeAttribute("data-task-id");
   document.querySelector("#save-task").textContent = "Add Task";
 }
 
+//create a new list item and setting div, attributes, id
   var createTaskEl = function(taskDataObj){
   //create list item
   var listItemEl = document.createElement("li"); 
@@ -64,6 +77,10 @@ var completeEditTask = function(taskName, taskType, taskId) {
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl); 
 
+  taskDataObj.id = taskIdCounter;
+
+  tasks.push(taskDataObj);
+
   var taskActionsEl = createTaskActions(taskIdCounter);
   listItemEl.appendChild(taskActionsEl);
 
@@ -76,6 +93,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
   taskIdCounter++;
   }; 
 
+// create delete and edit buttons, status drop-down 
   var createTaskActions = function(taskId) {
   var actionContainerEl = document.createElement("div");
   actionContainerEl.className = "task-actions";
@@ -96,7 +114,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
 
   actionContainerEl.appendChild(deleteButtonEl);
 
-//create drop down menu for status
+  //create drop down menu for status
   var statusSelectEl = document.createElement("select");
   statusSelectEl.className = "select-status";
   statusSelectEl.setAttribute("name","status-change"); 
@@ -115,11 +133,11 @@ var completeEditTask = function(taskName, taskType, taskId) {
     statusSelectEl.appendChild(statusOptionElement);
   }
 
-
   return actionContainerEl;
 
   };
 
+// add functionality to buttons when clicked
   var taskButtonHandler = function(event) {
     //get target element from event
     var targetEl = event.target;
@@ -141,6 +159,20 @@ var completeEditTask = function(taskName, taskType, taskId) {
   var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
+
+    //create new array to hold updated list of tasks
+    var updatedTaskArr = [];
+
+    //loop through current tasks
+    for (var i; i < tasks.length; i++) {
+      //if tasks[i].id doesnt match the value of taskId, let's keep that task and push it into the new array
+      if (task[i].id !== parseInt(taskId)) {
+        updatedTaskArr.push(tasks[i]);
+      }
+    }
+
+    //reassing tasks array to be the same as updatedTaskArr
+    tasks = updatedTaskArr;
   };
 
   var editTask = function(taskId) {
@@ -160,6 +192,7 @@ var completeEditTask = function(taskName, taskType, taskId) {
     formEl.setAttribute("data-task-id", taskId); 
   };
 
+// changing the status of a task and moving into different column
   var taskStatusChangeHandler = function(event) {
     //get the task item's id
     var taskId = event.target.getAttribute("data-task-id");
@@ -179,6 +212,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
     else if (statusValue === "completed") {
       tasksCompletedEl.appendChild(taskSelected);
     }
+
+    //update tasks in tasks array
+    for (var i = 0; i < tasks.length; i++) {
+      if (tasks[i].id ===parseInt(taskId)) {
+        tasks[i].status = statusValue;
+      }
+    }
+    console.log(tasks);
   };
 
   formEl.addEventListener("submit", taskFormHandler);
